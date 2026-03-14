6ea8584f59585c328add8ed6093cfdfb290f416a
0rion-jpg <aymeric.landy@gmail.com>
2026-03-14 20:48:27 +0100

    Delete assets (copie) directory

--- assets (copie)/js/search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#search-textarea").on("keypress", function (e) {
-        if (e.which === 13 && !e.shiftKey) {
-            e.preventDefault();
-            $(this).closest("form").submit();
-        }
-    });
-});
